bb7f8eaa797e35757820ac241e3fc1d7846acb25
KellyAllen013069 <89815777+KellyAllen013069@users.noreply.github.com>
2024-07-21 11:56:40 -0500

    Finished LINQ exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //going old school
+            var gameNames = new List<string>()
+            {
+                "Pac-Man",
+                "Donkey Kong",
+                "Galaga",
+                "Centipede",
+                "Star Wars",
+                "Dig Dug"
+            };
+
+            var orderedGameNames = gameNames.OrderBy(game => game.Length).ToList();
+
+            foreach (var game in orderedGameNames)
+            {
+                Console.WriteLine(game);
+            }
+            
         }
     }
 }
